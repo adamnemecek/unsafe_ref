74254543586ca669ea589ae97fcf1abf7ecd65b4
adamnemecek <adamnemecek@gmail.com>
2021-02-23 16:50:42 -0800

    unsafe_ref

--- src/ref_.rs
+++ src/ref_.rs
@@ -1,3 +1,28 @@
+use std::ops::Deref;
+
 pub struct UnsafeRef<T> {
     inner: *const T,
+}
+
+impl<T> UnsafeRef<T> {
+    pub fn new(inner: &T) -> Self {
+        Self {
+            inner
+        }
+    }
+}
+
+impl<T> Clone for UnsafeRef<T> {
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner
+        }
+    }
+}
+
+impl<T> Deref for UnsafeRef<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        unsafe { self.inner.as_ref().unwrap() }
+    }
 }
